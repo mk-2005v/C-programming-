8db60078df104366780cdc6a32361bf76ade181e
kaan.uyar <kaan.uyar@cstech.com.tr>
2024-05-03 13:31:53 +0300

    docs: comments have been arranged!
    test: asserts have been added!

--- numerical_methods/square_root.c
+++ numerical_methods/square_root.c
@@ -1,77 +1,120 @@
-#include <stdio.h>
-#include <float.h>
+/**
+ * @file
+ * @brief a Newton-Raphson method and square-root implementation
+ * https://en.wikipedia.org/wiki/Newton%27s_method
+ * @details
+ * square-root implementation by using newton raphson method
+ * @author [Ali Kaan UYAR](https://github.com/kaan2463)
+ */
 
-#define NR_DE               1.0e-8
-#define NR_MAX_ITER         55
+
+#include <stdio.h> //for printf
+#include <assert.h> // for assert
+
+// Epsilon value 0.00000001 (Delta E)
+#define NR_DE               1.0e-8 
+// Max iteration of nr algorithm
+#define NR_MAX_ITER         55 
 
 
-typedef double (*DFUNC2D) (double, double);
-typedef double (*DFUNC1D) (double);
+typedef double (*DFUNC2D) (double, double); // functional type that is used as f in newton raphson algorithm
+typedef double (*DFUNC1D) (double); // functional type that is used as derivative of f
 
+/**
+ * @brief absolute difference between two double
+ * @param a : number1
+ * @param b : number2
+ * @returns |a-b|
+ */
 double abs_diff(double a, double b)
 {
-    return a > b? a - b: b - a;
+	return a > b ? a - b : b - a;
 }
 
-/*
-* Newton Raphson Method
-* use to find point that intersect to x-axis
-* for f(x) = y ==> (x0,0)
-* f  : function (callback)  = cx^t + a
-* df : derivative of f
-* iteration: x1 = x0 - f(x0)/df(x0)
+/**
+* @brief Newton Raphson Method (nr)
+* @param a : number that is contant in f (ex: f = x^2 -a)
+* @param f : function f that algortihm use
+* @param df : derivative of f
+* @returns point that f intersect to x-axis
 */
 double nr(double a, DFUNC2D f, DFUNC1D df)
 {
-    // initial point is chosen 1.0, can be any number
-    static const double initial_point = 1.0; 
-    int iter = 0;
-    double old_x;
-    double x = initial_point; 
-    do{
-        old_x = x;
-        x = x - f(x,a) / df(x);
-        iter++;
-    }while(iter < NR_MAX_ITER && NR_DE < abs_diff(old_x, x) );
-    return x;
+	// initial point is chosen 1.0, it can be any positive number
+	static const double initial_point = 1.0;
+	int iter = 0;
+	double old_x;
+	double x = initial_point;
+	do
+	{
+		old_x = x;
+		x = x - f(x, a) / df(x);
+		iter++;
+	} while (iter < NR_MAX_ITER && NR_DE < abs_diff(old_x, x));
+	return x;
 }
 
-// root of parabola is actually a sqrt of a
+/**
+ * @brief parabola function (x^2 - a)
+ * @param x : variable x
+ * @param a : constant number that shift parabola to -y direction
+ * @returns x^2 - a
+ */
 double parabola(double x, double a)
 {
-    return x*x - a;
+	return x * x - a;
 }
 
+/**
+ * @brief derivative of parabola
+ * @param x : variable x
+ * @returns 2*x
+ */
 double deriv_parabola(double x)
 {
-    return 2*x;
+	return 2 * x;
 }
 
-// sqrt of x
+/**
+ * @brief square root function, f,a = parabola will give us sqrt of a in nr-algorithm
+ * @param x : variable x
+ * @returns sqrt(x) (x^(1/2))
+ */
 double square_root(double x)
 {
-    return nr(x, &parabola, &deriv_parabola);
+	return nr(x, &parabola, &deriv_parabola);
 }
 
-void test_sqrt()
+/**
+ * @brief test function
+ * @returns void
+ */
+static void test()
 {
-    double numbers[] = {12, 4, 16, 3, 9, 49};
+	double numbers[] = {12, 4, 16, 3, 9, 49, 86, 34, 5467, 0.23, 0.0721}; // random numbers
 
-    size_t sz = sizeof(numbers) / sizeof(double);
+	size_t sz = sizeof(numbers) / sizeof(double); // size of array above
 
-    double root;
-    size_t i;
-    for(i = 0; i < sz; i++)
-    {
-        root = square_root(numbers[i]);
-        printf(" square_root(%lf) = %lf, square_abs_error = %lf \n", numbers[i], root, abs_diff(numbers[i], root * root));
-    }
-
+	double root; // calculated by nr-algorithm
+	double abs_error; // abs error between number and square of root ==> |a-(sqrt(a)*sqrt(a))|
+	size_t i;
+	for (i = 0; i < sz; i++)
+	{
+		root = square_root(numbers[i]);
+		abs_error = abs_diff(numbers[i], root * root);
+		printf("square_root(%lf) = %lf, square_abs_error = %lf \n", numbers[i], root, abs_error);
+		assert(abs_error < NR_DE); // if error is under DE = 1.0e-8 it passes the test
+	}
+	printf("\n\n");
+	printf("All tests have successfully passed!\n");
 }
 
-int main(){
-    
-    test_sqrt();
-
-    return 0;
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+int main()
+{
+	test();
+	return 0;
 }
--- numerical_methods/trigonometric_functions.c
+++ numerical_methods/trigonometric_functions.c
@@ -1,25 +1,39 @@
-/*
-* Trigonometric Functions 
-* implemented by using Taylor Series
-* @see https://en.wikipedia.org/wiki/Taylor_series
-*/
+/**
+ * @file
+ * @brief Trigonometric Functions
+ * implemented by using Taylor Series
+ * https://en.wikipedia.org/wiki/Taylor_series
+ * @details
+ * Trigonometric functions that are implemented by using Taylor Series
+ * They give us efficient result for 0 < x < pi/2.
+ * And for reverse func. they give us efficient result for 0 < x < pi/4.
+ * Values other than this interval can easily implemented by trigonometric conversions.
+ * @author [Ali Kaan UYAR](https://github.com/kaan2463)
+ */
 
+#include <stdio.h> // for printf
+#include <math.h> // for std trigonometric functions to compare
+#include <assert.h> // for assert
 
-#include <stdio.h>
-#include <math.h>
+// Max depth in Taylor series
+#define TRIG_MAX_ITER   70
+// PI constant
+#define TRIG_PI         3.14159265358979323846 
 
-#define TRIG_MAX_ITER   21
-#define TRIG_PI         3.14159265358979323846
-
+/**
+ * @brief sinus function
+ * @param x : variable
+ * @returns sin(x)
+ */
 double trig_sin(double x)
 {
     double pow_x = 1.0;
     double fac = 1.0;
     double result = 0.0;
     double sign;
-    for(size_t i = 0; i < TRIG_MAX_ITER - 1; i++)
+    for (size_t i = 0; i < TRIG_MAX_ITER - 1; i++)
     {
-        if(i % 2 == 1)
+        if (i % 2 == 1)
         {
             sign = ((i - 1) / 2) % 2 == 0 ? 1.0 : -1.0;
             result += sign * pow_x / fac;
@@ -30,15 +44,20 @@ double trig_sin(double x)
     return result;
 }
 
+/**
+ * @brief cosinus function
+ * @param x : variable
+ * @returns cos(x)
+ */
 double trig_cos(double x)
 {
     double pow_x = 1.0;
     double fac = 1.0;
     double result = 0.0;
     double sign;
-    for(size_t i = 0; i < TRIG_MAX_ITER; i++)
+    for (size_t i = 0; i < TRIG_MAX_ITER; i++)
     {
-        if(i % 2 == 0)
+        if (i % 2 == 0)
         {
             sign = (i / 2) % 2 == 0 ? 1.0 : -1.0;
             result += sign * pow_x / fac;
@@ -49,7 +68,12 @@ double trig_cos(double x)
     return result;
 }
 
-
+/**
+ * @brief tangent function
+ * not efficient
+ * @param x : variable
+ * @returns tan(x)
+ */
 double trig_tan(double x)
 {
     double pow_x = 1.0;
@@ -58,15 +82,15 @@ double trig_tan(double x)
     double result_cos = 0.0;
     double sign_sin;
     double sign_cos;
-    for(size_t i = 0; i < TRIG_MAX_ITER; i++)
+    for (size_t i = 0; i < TRIG_MAX_ITER; i++)
     {
-        if(i % 2 == 1)
+        if (i % 2 == 1)
         {
             sign_sin = ((i - 1) / 2) % 2 == 0 ? 1.0 : -1.0;
             result_sin += sign_sin * pow_x / fac;
         }
 
-        if(i % 2 == 0)
+        if (i % 2 == 0)
         {
             sign_cos = (i / 2) % 2 == 0 ? 1.0 : -1.0;
             result_cos += sign_cos * pow_x / fac;
@@ -77,6 +101,12 @@ double trig_tan(double x)
     return result_sin / result_cos;
 }
 
+/**
+ * @brief reverse sinus function
+ * not efficient
+ * @param x : variable
+ * @returns arcsin(x)
+ */
 double trig_arcsin(double x)
 {
     double pow_x = 1.0;
@@ -84,15 +114,15 @@ double trig_arcsin(double x)
     double fac = 1.0;
     double fac2n = 1.0;
     double result = 0.0;
-    for(size_t i = 0; i < TRIG_MAX_ITER + 1; i++)
+    for (size_t i = 0; i < TRIG_MAX_ITER + 1; i++)
     {
-        if(i > 1)
+        if (i > 1)
         {
             fac2n *= (double)(i - 1);
         }
-        if(i % 2 == 1)
+        if (i % 2 == 1)
         {
-            if(i > 1)
+            if (i > 1)
             {
                 fac *= ((double)(i - 1)) / 2.0;
             }
@@ -105,19 +135,20 @@ double trig_arcsin(double x)
     return result;
 }
 
-double trig_arccos(double x)
-{
-    return (TRIG_PI / 2.0) - trig_arcsin(x);
-}
-
+/**
+ * @brief reverse tangent function
+ * not efficient
+ * @param x : variable
+ * @returns arctan(x)
+ */
 double trig_arctan(double x)
 {
     double pow_x = 1.0;
     double result = 0.0;
     double sign;
-    for(size_t i = 0; i < TRIG_MAX_ITER - 1; i++)
+    for (size_t i = 0; i < TRIG_MAX_ITER - 1; i++)
     {
-        if(i % 2 == 1)
+        if (i % 2 == 1)
         {
             sign = ((i - 1) / 2) % 2 == 0 ? 1.0 : -1.0;
 
@@ -128,58 +159,113 @@ double trig_arctan(double x)
     return result;
 }
 
+//TEST
 
-
-double abs_diff(double a, double b)
-{
-    return a > b? a - b: b - a;
-}
-
-typedef double (*trig_f)(double);
-
+/**
+ * @brief absolute difference between two double
+ * @param a : number1
+ * @param b : number2
+ * @returns |a-b|
+ */
+ double abs_diff(double a, double b)
+ {
+     return a > b ? a - b : b - a;
+ }
+ 
+typedef double (*trig_f)(double); //trigonometric function type
+ 
+// help to arrange test params
 #define test_param(x) &trig_ ## x, &x, #x
-void test_func(double rad, trig_f f, trig_f sf, char *f_str)
-{
-    double trig_result = f(rad);
-    double std_result = sf(rad);
-    printf("trig_%s(%lf) = %lf, std_%s(%lf) = %lf, abs_error = %lf \n",
-            f_str, rad, trig_result,
-            f_str, rad, std_result,
-            abs_diff(trig_result, std_result));
-}
+// Epsilon value 0.000001
+#define TEST_DE 1.0e-6 
 
+/**
+ * @brief test helper function for sin, cos, tan
+ * @param rad : radian
+ * @param f : functional (trig_sin, trig_cos, trig_tan)(implemented func.)
+ * @param sf : std functional (sin, cos, tan)
+ * @param f_str : name of function
+ * @return void
+ */
+ void test_func(double rad, trig_f f, trig_f sf, char* f_str)
+ {
+     double trig_result = f(rad); // trigonometric function result 
+     double std_result = sf(rad); // std trigonometric function result
+     double abs_error; // difference between implemeted func. and std func.
 
-// TODO kaan: reverse trigonometric functions' test
-// compare implementation and built in std trig. func. results
-void test()
-{
-    // 0 < rad < pi/2
-    double radians []= {0.33, 0.47, 0.44, 0.25, 0.12, 0.2};
+     abs_error = abs_diff(trig_result, std_result);
+     printf("trig_%s(%lf) = %lf, std_%s(%lf) = %lf, abs_error = %lf \n",
+         f_str, rad, trig_result,
+         f_str, rad, std_result,
+         abs_error);
+     assert(abs_error < TEST_DE);
+ }
 
-    size_t sz = sizeof(radians) / sizeof(double);
+ /**
+ * @brief test : compare implemented functions and std function
+ * @return void
+ */
+ void test()
+ {
+     // 0 < rad < pi/2
+     double radians[] = {0.33, 0.47, 0.44, 0.25, 0.12, 0.2};
+ 
+     size_t sz = sizeof(radians) / sizeof(double);
+ 
+     double rad;
+     size_t i;
+     for (i = 0; i < sz; i++)
+     {
+         rad = radians[i] * TRIG_PI;
+         test_func(rad, test_param(sin));
+     }
+ 
+     for (i = 0; i < sz; i++)
+     {
+         rad = radians[i] * TRIG_PI;
+         test_func(rad, test_param(cos));
+     }
+ 
+     for (i = 0; i < sz; i++)
+     {
+         rad = radians[i] * TRIG_PI;
+         test_func(rad, test_param(tan));
+     }
 
-    double rad;
-    size_t i;
-    for(i=0; i < sz; i++)
-    {
-        rad = radians[i] * TRIG_PI;
-        test_func(rad, test_param(sin));
-    }
+     // 0 < rad < pi/2
+     double radians2[] = {0.1, 0.229, 0.22, 0.019, 0.123};
 
-    for(i=0; i < sz; i++)
-    {
-        rad = radians[i] * TRIG_PI;
-        test_func(rad, test_param(cos));
-    }
+     size_t sz2 = sizeof(radians2) / sizeof(double);
 
-    for(i=0; i < sz; i++)
-    {
-        rad = radians[i] * TRIG_PI;
-        test_func(rad, test_param(tan));
-    }
+     double abs_error; // abs error func between actual value and reverse trigonometric functions
+     double result;
+     for (i = 0; i < sz2; i++)
+     {
+         rad = radians2[i] * TRIG_PI;
+         result = trig_arcsin(trig_sin(rad));
+         abs_error = abs_diff(rad, result);
+         printf("arcsin(sin(%lf)) = %lf, abs_error = %lf\n", rad, result, abs_error);
+         assert(abs_error <TEST_DE);
+     }
 
-}
+     for (i = 0; i < sz2; i++)
+     {
+         rad = radians2[i] * TRIG_PI;
+         result = trig_arctan(trig_tan(rad));
+         abs_error = abs_diff(rad, result);
+         printf("arctan(tan(%lf)) = %lf, abs_error = %lf\n", rad, result, abs_error);
+         assert(abs_error < TEST_DE);
+     }
 
+     printf("\n\n");
+     printf("All tests have successfully passed!\n");
+ 
+ }
+
+ /**
+ * @brief Main function
+ * @returns 0 on exit
+ */
 int main()
 {
     test();
